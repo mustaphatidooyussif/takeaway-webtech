656498c6d854e247b0f6b73a616e792c361169c0
mustaphatidooyussif <mustaphatidooyussif@yahoo.com>
2018-10-30 18:49:46 -0700

    added alert page

---
+++ customer-order-alert.php
@@ -0,0 +1,14 @@
+<?php include('template-parts/admin-header.php'); ?>
+
+    <div style="background-color:#eeeeee;"> <!-- WRAPPER -->
+
+       <?php include('template-parts/atule-customer-navbar.php'); ?>
+        <div>
+            <div>
+            </div>
+            <div>
+            </div>
+        </div>
+    <!-- </div>END WRAPPER DIV -->
+
+<?php include('template-parts/atule-customer-footer.php'); ?>
