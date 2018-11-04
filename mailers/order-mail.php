d5cda72c53b3578d04ec5d07f31c3f8d0e296523
Samuel Atule <atulesamuel20@gmail.com>
2018-11-04 00:15:48 +0000

    .

--- mailers/order-mail.php
+++ mailers/order-mail.php
@@ -1,31 +1,48 @@
 <?php
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 // set path to look
 set_include_path('C:/xampp/htdocs"/takeaway-webtech/connection/');
 // import file
 require_once get_include_path()."initDatabase.php";
 $db = new InitDatabase();  //create db and tables if not exists
 $db->createDataBaseTables();
+
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 //Load Composer's autoloader
 require 'vendor/autoload.php';
 
+
+    $orders_id = unserialize($_POST['orders_ids']);
+    $total_price=$_POST['total_price'];
+    // loop crreate tr for each order id
+    $tr_data = '';
+    foreach($orders_id as $id){ 
+        $tr_data .= '<tr>
+                        <td>%food_item'.$id.'%</td>
+                        <td>%item_price'.$id.'%</td>
+                    </tr>';
+
+    }
+
+$table = '<table style="width: 100%;border: 1px solid black;">
+            <tr>
+                <th>Item</th>
+                <th>Price</th>
+            </tr>'.$tr_data
+        .'</table>';
+
 // Retrieve the email template required
-$message = file_get_contents('templates/order-email-template.php');
+$message = file_get_contents('templates/order-email-template.html' );
 $username = 'Samuel Atule';
 
 // Replace the % with the actual information
 $message = str_replace('%username%', $username, $message);
-// get data from submitted form through php curl
-$orders_data = explode('&', $_POST['orders_data']);
+$message = str_replace('%table%', $table, $message);
+$message = str_replace('%total_price%', $total_price, $message);
 
-$total_price = $orders_data[0];
-$orders_id = array_slice($orders_data, 1);
-print_r($total_price);
-print_r($orders_id);
 // Loop through and retrieve food items into food message
 foreach($orders_id as $id) {
     $orders = $db->selectItemByColumn($db->ak_orders_table, $db->orders_id, $id);
@@ -39,17 +56,18 @@ foreach($orders_id as $id) {
     $row = $unserved_food_item->fetch();
     // retrieve food items into food message
     $message = str_replace('%food_item'.$id.'%', $row["food_item"], $message);  // Get food items from database
-    $message = str_replace('%item_price'.$id.'%', $row["price"], $message);  // Get item price from database
+    $message = str_replace('%item_price'.$id.'%', $row["price"], $message);  // Get item price from database  
+
 }
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
-    // $mail->SMTPDebug = 1;                                 // Enable verbose debug output
+    $mail->SMTPDebug = 1;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';  //Sendgrid:smtp.sendgrid.net// Specify main and backup SMTP servers
+    $mail->Host = 'smtp.sendgrid.net';  //Sendgrid:smtp.sendgrid.net// smtp.gmail.com Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'takeawayproject112@gmail.com';                //Google: takeawayproject112@gmail.com  // SMTP username
+    $mail->Username = 'takeawayproject112';                //Google: takeawayproject112@gmail.com  // SMTP username
     $mail->Password = 'takeawayproject';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
@@ -74,7 +92,7 @@ try {
 
     $url = $_SERVER['DOCUMENT_ROOT'].'/takeaway-webtech/customer-dashboard.php'; 
     // $mail->send();
-    header("Location:../customer-food-menu.php"); 
+    // header("Location:../customer-food-menu.php"); 
     
     echo 'Message has been sent ';
 } catch (Exception $e) {
